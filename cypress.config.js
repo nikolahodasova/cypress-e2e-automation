7b6f2cf348d1cab7021e79d28bbea2e464b39082
Nikola Hodasova <nikola.hodasova.projects@gmail.com>
2026-02-20 20:34:47 +0100

    Cypress config prepis

--- cypress.config.js
+++ cypress.config.js
@@ -6,27 +6,6 @@ module.exports = defineConfig({
     reporter: 'mocha-junit-reporter',
     reporterOptions: {mochaFile: 'cypress/results/results-[hash].xml'},
     setupNodeEvents(on, config) {
-      // Aktivácia pluginu
-      addXrayResultUpload(on, config);
       return config;
-    },
-    env: {
-      xray: {
-        // Tu zadáš údaje, ktoré dostaneš z Jiry
-        projectKey: "TVOJ_PROJEKT_KEY", 
-        uploadResults: true,
-        cloud: {
-          clientId: "TVOJE_CLIENT_ID",
-          clientSecret: "TVOJ_CLIENT_SECRET"
-        }
-      }
     }
 }})
-const { defineConfig } = require("cypress");
-const { addXrayResultUpload } = require("cypress-xray-plugin/plugin"); // Pridaj toto
-
-module.exports = defineConfig({
-  e2e: {
-    
-  }
-});
